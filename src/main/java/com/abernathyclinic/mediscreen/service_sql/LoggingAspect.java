c9779be0bf65943befa3881661f21ab5b169d9e7
Damien <damiencampagnapro@gmail.com>
2021-04-19 13:11:40 +0200

    Change of the Aspect class to resolve an issue in which the logging proceeded by an @Around was disabling the send of the default Spring Boot json message f.e in case of a wrong url

--- src/main/java/com/abernathyclinic/mediscreen/service_sql/LoggingAspect.java
+++ src/main/java/com/abernathyclinic/mediscreen/service_sql/LoggingAspect.java
@@ -1,9 +1,12 @@
 package com.abernathyclinic.mediscreen.service_sql;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,25 +21,32 @@ public class LoggingAspect {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-	@Around("execution(* com.abernathyclinic.mediscreen.service_sql.controller.*.*(..))")
-	public Object logAllControllers(ProceedingJoinPoint proceedingJoinPoint) {
-		MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
-
-		// Retrieve the details of the intercepted method
-		String className = methodSignature.getDeclaringType().getSimpleName();
-		String methodName = methodSignature.getName();
-
-		Object result = null;
-		try {
-			result = proceedingJoinPoint.proceed();
-		} catch (Throwable e) {
-			logger.info("The execution of {} in the {} class have failed.", methodName, className);
-			e.printStackTrace();
-		}
-
-		logger.info("Execution of {} in the {} class.", methodName, className);
-
-		return result;
+	@Pointcut("execution(* com.abernathyclinic.mediscreen.service_sql.controller.*.*(..))")
+	protected void controller() {
+		// Allow us to write once and for all the package and/or class targeted with the
+		// differents advices, making the path clearer in the advices definition.
 	}
 
+	@Before("controller()")
+	public void logBeforeTheMethod(JoinPoint joinPoint) {
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+
+		logger.info("Execution of {} in the {} class", methodName, className);
+	}
+
+	@AfterReturning("controller()")
+	public void logAfterTheMethodEndWithASuccess(JoinPoint joinPoint) {
+		logger.info("The execution has ended successfully");
+	}
+
+	@AfterThrowing(pointcut = "controller()", throwing = "userNotFoundException")
+	public void logAfterTheMethodEndWithAnError(JoinPoint joinPoint, UserNotFoundException userNotFoundException) {
+		Signature signature = joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+
+		logger.info("The execution of {} in the {} class has failed", methodName, className, userNotFoundException);
+	}
 }
